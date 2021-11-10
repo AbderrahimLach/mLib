470f318e2070e150227faf91486e1de306c5bff1
BuildTools <unconfigured@null.spigotmc.org>
2021-11-10 22:54:16 +0200

    Commands Huge Update, Updated ItemBuilder.java

---
+++ src/main/java/dev/mqzn/lib/commands/test/TestCommand.java
@@ -0,0 +1,21 @@
+package dev.mqzn.lib.commands.test;
+
+import dev.mqzn.lib.commands.api.MCommand;
+import dev.mqzn.lib.commands.api.Requirement;
+
+import java.util.Set;
+
+public class TestCommand extends MCommand {
+
+    public TestCommand(String... aliases) {
+        super("test", "test.perms", "test shit", "/test",
+                true, "tez");
+    }
+
+    @Override
+    public Set<Requirement> setRequirements() {
+        return null;
+    }
+
+
+}
